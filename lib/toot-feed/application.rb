3458d9cc4b1bcd175e43ddb76d438411eed72e71
User Pooza <pooza@dodok.b-shock.co.jp>
2017-10-16 18:09:46 +0900

    fixes #8

--- lib/toot-feed/application.rb
+++ lib/toot-feed/application.rb
@@ -106,14 +106,14 @@ module TootFeed
       return @logger
     end
 
-    def result_xml (message)
+    def result_xml (result)
       xml = REXML::Document.new
       xml.add(REXML::XMLDecl.new('1.0', 'UTF-8'))
       xml.add_element(REXML::Element.new('result'))
       status = xml.root.add_element('status')
-      status.add_text(message[:response][:status].to_s)
+      status.add_text(result[:response][:status].to_s)
       message = xml.root.add_element('message')
-      message.add_text(message[:response][:message] || 'error')
+      message.add_text(result[:response][:message] || 'error')
       return xml
     end
 
