e419a5e62d97b2c9b956d7a66260dbe29fafd1f9
User Pooza <pooza@dodok.b-shock.local>
2018-01-20 19:12:04 +0900

    refs #22

--- lib/radish-feed/tweet.rb
+++ lib/radish-feed/tweet.rb
@@ -1,7 +1,40 @@
+require 'uri'
+require 'zlib'
+
 module RadishFeed
   class Tweet < String
+    TOOT_LENGTH = 500
+    FULL_LENGTH = 140
+    URI_LENGTH = 24
+
     def tweetable_text
-      return String.new(self)
+      links = {}
+      text = String.new(self)
+      URI.extract(text, ['http', 'https']).each do |link|
+        if max_length < text.index(link)
+          text.sub!(link, '')
+        else
+          key = Zlib.crc32(text)
+          links[key] = link
+          text.sub!(link, create_tag(key))
+        end
+      end
+      if max_length < text.length
+        text = text.slice(max_length..TOOT_LENGTH) + '…'
+      end
+      links.each do |key, link|
+        text.sub!(create_tag(key), link)
+      end
+      return text
+    end
+
+    private
+    def max_length
+      return FULL_LENGTH - URI_LENGTH - 2
+    end
+
+    def create_tag (key)
+      return sprintf('{crc:%0' + (URI_LENGTH - 9).to_s + 'd}', key)
     end
   end
 end
