bf4c3c3d43fa3898f6acb4044bd24432a5297f23
Tatsuya Koishi <tkoishi@b-shock.co.jp>
2018-02-09 11:16:17 +0900

    refs #32

--- lib/radish-feed/application.rb
+++ lib/radish-feed/application.rb
@@ -8,6 +8,8 @@ require 'radish-feed/xml'
 
 module RadishFeed
   class Application < Sinatra::Base
+    attr :renderer, true
+
     def initialize
       super
       @config = Config.new
@@ -26,68 +28,68 @@ module RadishFeed
 
     before do
       @message = {request:{path: request.path, params:params}, response:{}}
-      @renderer = XML.new
+      renderer = XML.new
     end
 
     after do
-      @message[:response][:status] ||= @renderer.status
-      if (@renderer.status < 300)
+      @message[:response][:status] ||= renderer.status
+      if (renderer.status < 300)
         @logger.info(@message.to_json)
       else
         @logger.error(@message.to_json)
       end
-      status @renderer.status
-      content_type @renderer.type
+      status renderer.status
+      content_type renderer.type
     end
 
     get '/about' do
-      @message[:response][:status] = @renderer.status
+      @message[:response][:status] = renderer.status
       @message[:response][:message] = '%s %s'%([
         @config['application']['name'],
         @config['application']['version'],
       ])
-      return @renderer.generate(@message).to_s
+      return renderer.generate(@message).to_s
     end
 
     get '/feed/v1.1/account/:account' do
       unless registered?(params[:account])
-        @renderer.status = 404
-        @message[:response][:status] = @renderer.status
+        renderer.status = 404
+        @message[:response][:status] = renderer.status
         @message[:response][:message] = "Account #{params[:account]} not found."
-        return @renderer.generate(@message).to_s
+        return renderer.generate(@message).to_s
       end
-      @renderer = Atom.new
-      @renderer.tweetable = true
-      @renderer.tweetable = params[:tweetable]
-      @renderer.title_length = params[:length]
-      return @renderer.generate(
+      renderer = Atom.new
+      renderer.tweetable = true
+      renderer.tweetable = params[:tweetable]
+      renderer.title_length = params[:length]
+      return renderer.generate(
         'account_timeline',
         [params[:account], params[:entries].to_i]
       ).to_s
     end
 
     get '/feed/v1.1/local' do
-      @renderer = Atom.new
-      @renderer.tweetable = params[:tweetable]
-      @renderer.title_length = params[:length]
-      return @renderer.generate(
+      renderer = Atom.new
+      renderer.tweetable = params[:tweetable]
+      renderer.title_length = params[:length]
+      return renderer.generate(
         'local_timeline',
         [params[:entries].to_i]
       ).to_s
     end
 
     not_found do
-      @renderer.status = 404
-      @message[:response][:status] = @renderer.status
+      renderer.status = 404
+      @message[:response][:status] = renderer.status
       @message[:response][:message] = "Resource #{@message[:request][:path]} not found."
-      return @renderer.generate(@message).to_s
+      return renderer.generate(@message).to_s
     end
 
     error do
-      @renderer.status = 500
-      @message[:response][:status] = @renderer.status
+      renderer.status = 500
+      @message[:response][:status] = renderer.status
       @message[:response][:message] = env['sinatra.error'].message
-      return @renderer.generate(@message).to_s
+      return renderer.generate(@message).to_s
     end
 
     private
