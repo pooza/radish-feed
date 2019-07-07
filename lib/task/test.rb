7d7ab2ec01627b3d159066bb7e6de18325da4339
Tatsuya Koishi <tkoishi@b-shock.co.jp>
2019-07-07 15:40:42 +0900

    ginseng-core 1.4.3

--- lib/task/test.rb
+++ lib/task/test.rb
@@ -1,9 +1,8 @@
-namespace :radish do
-  task :test do
-    ENV['TEST'] = RadishFeed::Package.name
-    require 'test/unit'
-    Dir.glob(File.join(RadishFeed::Environment.dir, 'test/*')).each do |t|
-      require t
-    end
+desc 'test all'
+task :test do
+  ENV['TEST'] = RadishFeed::Package.name
+  require 'test/unit'
+  Dir.glob(File.join(RadishFeed::Environment.dir, 'test/*')).each do |t|
+    require t
   end
 end
