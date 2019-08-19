125e183bcf4f5bbb2bad91dbc9ea07e5dcf71fbc
Tatsuya Koishi <tkoishi@b-shock.co.jp>
2019-08-19 09:12:36 +0900

    ginseng-core 1.4.5

--- lib/radish_feed/package.rb
+++ lib/radish_feed/package.rb
@@ -1,35 +1,31 @@
 module RadishFeed
   module Package
-    def module_name
-      return 'RadishFeed'
-    end
-
     def environment_class
-      return "#{module_name}::Environment".constantize
+      return Environment
     end
 
     def package_class
-      return "#{module_name}::Package".constantize
+      return Package
     end
 
     def config_class
-      return "#{module_name}::Config".constantize
+      return Config
     end
 
     def logger_class
-      return "#{module_name}::Logger".constantize
+      return Logger
     end
 
     def database_class
-      return "#{module_name}::Postgres".constantize
+      return Postgres
     end
 
     def http_class
-      return "#{module_name}::HTTP".constantize
+      return HTTP
     end
 
     def query_template_class
-      return "#{module_name}::QueryTemplate".constantize
+      return QueryTemplate
     end
 
     def self.name
